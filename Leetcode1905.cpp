855a439c06312fcfac39a083faab67d17fca3d5e
bhardwaj-shubham12 <50978992+bhardwaj-shubham12@users.noreply.github.com>
2024-10-04 21:38:03 +0530

    Create Leetcode1905.cpp

---
+++ Leetcode1905.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    bool flag = true;
+    int n,m;
+    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2,int row,int col)
+    {
+        if(row<0 || col<0 || row>=m || col >= n || !grid2[row][col]) return;
+        if(!grid1[row][col]) flag = 0;
+
+        grid1[row][col] = 0;
+        grid2[row][col] = 0;
+        
+        dfs(grid1,grid2,row+1,col);
+        dfs(grid1,grid2,row-1,col);
+        dfs(grid1,grid2,row,col-1);
+        dfs(grid1,grid2,row,col+1);
+    }
+public:
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        m = grid1.size();
+        n = grid1[0].size();
+        int count = 0;
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid2[i][j])
+                {
+                    flag = true;
+                    dfs(grid1,grid2,i,j);
+                    count += flag;
+                }
+            }
+        }
+
+        return count;
+    }
+};
