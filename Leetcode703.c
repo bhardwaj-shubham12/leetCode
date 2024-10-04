15a5cb21d0161473243b287d8a6f104da6994464
bhardwaj-shubham12 <50978992+bhardwaj-shubham12@users.noreply.github.com>
2024-10-04 21:13:22 +0530

    Create Leetcode703.c

---
+++ Leetcode703.c
@@ -0,0 +1,43 @@
+class KthLargest {
+    int K;
+    int size;
+    multiset<int> mset;
+public:
+    KthLargest(int k, vector<int>& nums) {
+      /*
+       Write code here
+        size = 0;
+        K = k;
+
+        for(int ele: nums)
+        {
+            mset.insert(-1 * ele);
+            size++;
+
+            if(size == K+1)
+            {
+                mset.erase(--mset.end());
+                size--;
+            }
+        }
+    }
+    */
+    int add(int val) {
+        mset.insert(-1 * val);
+        size++;
+
+        if(size == K+1)
+        {
+            mset.erase(--mset.end());
+            size--;
+        }
+
+        return -1 * (*mset.rbegin());
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
