f66f328964ab78dc0661a94c30be81c2024f7091
bhardwaj-shubham12 <50978992+bhardwaj-shubham12@users.noreply.github.com>
2026-06-21 15:22:12 +0530

    Time: 9 ms (81.71%), Space: 84.5 MB (5.04%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,13 +1,40 @@
 class Solution {
-    public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
+    private int[] countingSort(int[] costs){
+        int[] ans = new int[costs.length];
+        
+        int maxVal = costs[0];
+        for(int i=1;i<costs.length;i++){
+            maxVal = (costs[i] > maxVal) ? costs[i]: maxVal;
+        }
 
-        int res = 0;
+        int[] counter = new int[maxVal + 1];
+        for(int i=0;i<=maxVal;i++){
+            counter[i] = 0;
+        }
+
+        for(int i=0;i<costs.length;i++){
+            counter[costs[i]]++;
+        }
+
         int index = 0;
+        for(int i=1;i<=maxVal;i++){
+            for(int j=0;j<counter[i];j++){
+                ans[index++] = i;
+            }
+        }
 
-        while((index < costs.length) && (coins >= costs[index])){
+        return ans;
+
+    }
+    public int maxIceCream(int[] costs, int coins) {
+        int[] sortedCost = countingSort(costs);
+        
+        int index = 0;
+        int res = 0;
+
+        while((index < sortedCost.length) && (sortedCost[index] <= coins)){
+            coins -= sortedCost[index++];
             res++;
-            coins -= costs[index++];
         }
 
         return res;
