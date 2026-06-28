f1e771e4c1f8ede993f2f66e2dfcb39960aa9393
bhardwaj-shubham12 <50978992+bhardwaj-shubham12@users.noreply.github.com>
2026-06-28 16:29:36 +0530

    Time: 54 ms (10.77%), Space: 49.4 MB (86.29%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,34 +1,73 @@
+import java.util.*;
+
 class Solution {
-    public List<List<String>> accountsMerge(List<List<String>> acts) {
-        Map<String, String> owner = new HashMap<>();
+
+    // Fix: Use .equals() for String comparison
+    private String find(String s, Map<String, String> p) {
+        if (p.get(s).equals(s)) {
+            return s;
+        }
+        // Path compression: optimizes future lookups
+        p.put(s, find(p.get(s), p)); 
+        return p.get(s);
+    }
+
+    // Proper Union: Connects the root of one to the root of the other
+    private void union(String s1, String s2, Map<String, String> p) {
+        String root1 = find(s1, p);
+        String root2 = find(s2, p);
+        if (!root1.equals(root2)) {
+            p.put(root1, root2); 
+        }
+    }
+
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
         Map<String, String> parents = new HashMap<>();
-        Map<String, TreeSet<String>> unions = new HashMap<>();
-        for (List<String> a : acts) {
-            for (int i = 1; i < a.size(); i++) {
-                parents.put(a.get(i), a.get(i));
-                owner.put(a.get(i), a.get(0));
+        Map<String, TreeSet<String>> unionSet = new HashMap<>();
+        Map<String, String> owner = new HashMap<>();
+        List<List<String>> res = new ArrayList<>();
+
+        // 1. Initialize parents
+        for (List<String> account : accounts) {
+            for (int i = 1; i < account.size(); i++) {
+                String email = account.get(i);
+                if (!parents.containsKey(email)) {
+                    parents.put(email, email);
+                    owner.put(email, account.get(0));
+                }
             }
         }
-        for (List<String> a : acts) {
-            String p = find(a.get(1), parents);
-            for (int i = 2; i < a.size(); i++)
-                parents.put(find(a.get(i), parents), p);
+
+        // 2. Correct Union Step: Union all emails in an account to the first email
+        for (List<String> account : accounts) {
+            String firstEmail = account.get(1);
+            for (int i = 2; i < account.size(); i++) {
+                union(firstEmail, account.get(i), parents);
+            }
         }
-        for(List<String> a : acts) {
-            String p = find(a.get(1), parents);
-            if (!unions.containsKey(p)) unions.put(p, new TreeSet<>());
-            for (int i = 1; i < a.size(); i++)
-                unions.get(p).add(a.get(i));
+
+        // 3. Group by ultimate root parent
+        for (List<String> account : accounts) {
+            for (int i = 1; i < account.size(); i++) {
+                String parent = find(account.get(i), parents);
+                if (!unionSet.containsKey(parent)) {
+                    unionSet.put(parent, new TreeSet<>());
+                }
+                unionSet.get(parent).add(account.get(i));
+            }
         }
-        List<List<String>> res = new ArrayList<>();
-        for (String p : unions.keySet()) {
-            List<String> emails = new ArrayList(unions.get(p));
-            emails.add(0, owner.get(p));
-            res.add(emails);
+        
+        // 4. Build result
+        for (Map.Entry<String, TreeSet<String>> unionEle : unionSet.entrySet()) {
+            String parent = unionEle.getKey();
+            TreeSet<String> currentSet = unionEle.getValue();
+
+            ArrayList<String> temp = new ArrayList<>();
+            temp.add(owner.get(parent));
+            temp.addAll(currentSet);
+            res.add(temp);
         }
+
         return res;
-    }
-    private String find(String s, Map<String, String> p) {
-        return p.get(s) == s ? s : find(p.get(s), p);
     }
 }
