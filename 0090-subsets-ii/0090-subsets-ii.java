4e6a5727e2fd586f6139409c4df2b2169614a46b
bhardwaj-shubham12 <50978992+bhardwaj-shubham12@users.noreply.github.com>
2026-07-10 20:24:52 +0530

    Time: 5 ms (10.19%), Space: 45.4 MB (10.42%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        HashSet<List<Integer>> listSet = new HashSet<>();
+        int numsLen = nums.length;
+        int numSet = (int)Math.pow(2,numsLen);
+        //System.out.println(numSet);
+
+        for(int i=0;i<numSet;i++){
+            List<Integer> temp = new ArrayList<>();
+            //System.out.println("Set:"+i);
+            for(int j=0;j<numsLen;j++){
+                if((i & (1 << j)) > 0){
+                    //System.out.println("Adding " + j + " for set: "+i);
+                    temp.add(nums[j]);
+                }
+            }
+            if(!listSet.contains(temp))
+                ans.add(temp);
+                listSet.add(temp);
+        }
+
+        return ans;
+    }
+}
