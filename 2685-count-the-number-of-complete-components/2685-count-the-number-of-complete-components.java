c34037fe3fde408560e67450a1aeda26ba176b74
bhardwaj-shubham12 <50978992+bhardwaj-shubham12@users.noreply.github.com>
2026-07-11 12:42:41 +0530

    Time: 15 ms (19.79%), Space: 47.8 MB (39.49%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
@@ -0,0 +1,51 @@
+class Solution {
+    private void dfsUtil(List<List<Integer>>graph, int vertex, boolean[] visited, List<Integer> temp){
+        visited[vertex] = true;
+        temp.add(vertex);
+        for(int ele: graph.get(vertex)){
+            if(!visited[ele]){
+                dfsUtil(graph, ele, visited, temp);
+            }
+        }
+    }
+    public int countCompleteComponents(int n, int[][] edges) {
+        int numComp = 0;
+        boolean[] visited = new boolean[n];
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            graph.add(new ArrayList<>());
+        }
+
+        boolean[][] isEdgeConnect = new boolean[n][n];
+
+        for(int i=0;i<edges.length;i++){
+            graph.get(edges[i][0]).add(edges[i][1]);
+            graph.get(edges[i][1]).add(edges[i][0]);
+            isEdgeConnect[edges[i][0]][edges[i][1]] = true;
+            isEdgeConnect[edges[i][1]][edges[i][0]] = true;
+        }
+
+        List<Integer> temp;
+        boolean allConnect = true;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                temp = new ArrayList<>();
+                dfsUtil(graph, i, visited, temp);
+                allConnect = true;
+                for(int j=0;j<temp.size() - 1;j++){
+                    for(int k=j + 1; k< temp.size(); k++){
+                        if(!isEdgeConnect[temp.get(j)][temp.get(k)]){
+                            allConnect = false;
+                            break;
+                        }
+                    }
+                    if(!allConnect) break;
+                }
+                if(allConnect)
+                    numComp++;
+            }
+        }
+
+        return numComp;
+    }
+}
