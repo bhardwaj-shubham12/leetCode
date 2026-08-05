96dbacd48491e44f36707c30da20eff67dc98d3c
bhardwaj-shubham12 <50978992+bhardwaj-shubham12@users.noreply.github.com>
2026-08-05 07:48:58 +0530

    Time: 65 ms (68.27%), Space: 296.6 MB (10.58%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.java
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -1,30 +1,33 @@
 class Solution {
-    void dfsUtil(List<List<Integer>> graph, int curPoint, boolean[] isSus){
+    void dfsUtil(List<List<Integer>> graph, int curPoint, boolean[] isSus, int[] inDegree){
         isSus[curPoint] = true;
         //System.out.println(curPoint);
         for(int point: graph.get(curPoint)){
+            inDegree[point]--;
             if(!isSus[point]){
                 //System.out.println("Calling: "+point + " from: "+curPoint);
-                dfsUtil(graph,point, isSus);
+                dfsUtil(graph,point, isSus, inDegree);
             }
         }
     }
     public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
         List<List<Integer>> graph = new ArrayList<>();
-        List<List<Integer>> revGraph = new ArrayList<>();
+        
+        int[] inDegree = new int[n];
         for(int i=0;i<n;i++){
             graph.add(new ArrayList<>());
-            revGraph.add(new ArrayList<>());
         }
+
         for(int i=0;i<invocations.length;i++){
             graph.get(invocations[i][0]).add(invocations[i][1]);
-            revGraph.get(invocations[i][1]).add(invocations[i][0]);
+            inDegree[invocations[i][1]]++;
         }
+
         boolean[] isSus = new boolean[n];
         List<Integer> ans1 = new ArrayList<>();
         List<Integer> ans2 = new ArrayList<>();
 
-        dfsUtil(graph,k,isSus);
+        dfsUtil(graph,k,isSus,inDegree);
 
         for(int i=0;i<n;i++){
             ans1.add(i);
@@ -36,14 +39,14 @@ class Solution {
 
         for(int i=0;i<n;i++){
             if(isSus[i]){
-                for(int caller: revGraph.get(i)){
-                    if(!isSus[caller]){
-                        return ans1;
-                    }
+                if(inDegree[i] > 0){
+                    return ans1;
                 }
             }
         }
 
         return ans2;
+
+
     }
 }
